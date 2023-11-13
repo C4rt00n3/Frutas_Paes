11dc06442f9402d99bbf8ebae45f2ce22536673d
C4rt00n3 <rafaelcruzpaes@gmail.com>
2023-11-13 09:25:04 -0300

    fix/favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export const metadata: Metadata = {
   authors: [{ name: "Frutas Paes", url: "www.frutaspaes.com.br" }],
   description:
     "Frutas Paes - Venda online de manga e maracujá. Localizado em Livramento de Nossa Senhora, oferecemos produtos de alta qualidade e atendimento excepcional. Entregamos para todo o Brasil. Descubra o sabor fresco e a praticidade de comprar frutas conosco.",
+  icons: [{ rel: "icon", url: "./public/assets/favicon.ico" }],
 };
 
 export default function RootLayout({
